6c13b535b496f00700ff760d169ebd5fcd2a20f2
VitalyTVA <vitalytva@yandex.ru>
2014-10-01 12:06:52 +0400

    е

--- PureSharp/Monads/MayBeT.cs
+++ PureSharp/Monads/MayBeT.cs
@@ -40,22 +40,36 @@ namespace PureSharp.MayBeTransformer2 {
         public IMonad Monad { get; private set; } = monad;
     }
     public static partial class MayBeTExtensions2 {
-        //public static MayBeT<TMonad> Lift<TMonad>(TMonad value) {
-        //    return new MayBeT<TMonad>(value);
-        //}
+        public static TMonad RunMayBeT<A, TMonad>(this MayBeT<A> source) {
+            return (TMonad)source.InnerMonadicValue;
+        }
+        public static MayBeT<A> AsMayBeT<A>(this A source, IMonad monad) {
+            return source.Unit(monad);
+        }
         static MayBeT<A> Unit<A>(this A source, IMonad monad) {
             return new MayBeT<A>(monad.Unit(source.AsMayBe()), monad);
         }
         static MayBeT<B> SelectMany<A, B>(this MayBeT<A> source, Func<A, MayBeT<B>> f) {
-            return (MayBeT<B>)source.Monad.SelectMany<MayBe<A>, MayBe<B>>(
+            object result = source.Monad.SelectMany<MayBe<A>, MayBe<B>>(
                 source.InnerMonadicValue,
                 (MayBe<A> x) => {
                     if(x.Value != null) {
-                        return f(x.Value);
+                        return f(x.Value).InnerMonadicValue;
                     }
-                    return Unit<B>(default(B), source.Monad);
+                    return source.Monad.Unit(default(B).AsMayBe());
                 }
             );
+            return new MayBeT<B>(result, source.Monad);
         }
+
+        public static MayBeT<C> SelectMany<A, B, C>(this MayBeT<A> source, Func<A, MayBeT<B>> f, Func<A, B, C> resultSelector) {
+            return source.SelectMany(
+                outer => f(outer).SelectMany(
+                inner => resultSelector(outer, inner).Unit<C>(source.Monad)));
+        }
+        public static MayBeT<B> Select<A, B>(this MayBeT<A> source, Func<A, B> f) {
+            return source.SelectMany(x => f(x).Unit<B>(source.Monad));
+        }
+
     }
 }
