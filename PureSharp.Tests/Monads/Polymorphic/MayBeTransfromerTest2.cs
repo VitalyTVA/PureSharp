67e69d507cf8639f57bcdaed276357d53f833df1
Vitaly Tolstikov <vitalytva@yanex.ru>
2014-10-01 22:47:43 -0700

    t

---
+++ PureSharp.Tests/Monads/Polymorphic/MayBeTransfromerTest2.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using NUnit.Framework;
+using PureSharp.Tests.Utils;
+using PureSharp.MayBeTransformer2;
+using PureSharp.MayBeMonad2;
+using PureSharp.LazyMonad;
+
+namespace PureSharp.Tests {
+    [TestFixture]
+    public partial class MayBeTransfromerTests2 {
+        [Test]
+        public void MayBeTransfromerTest() {
+            int? value1 = null;
+            int? value2 = null;
+
+            var res = Sum(value1.AsMayBeT(new MayBeTransformer2.LazyMonad()), value2.AsMayBeT(new MayBeTransformer2.LazyMonad()));
+            res.RunMayBeT<int?, Lazy<MayBe<int?>>>().Value.Value.IsEqual<int?>(null);
+
+            value1 = 2;
+            res = Sum(value1.AsMayBeT(new MayBeTransformer2.LazyMonad()), value2.AsMayBeT(new MayBeTransformer2.LazyMonad()));
+            res.RunMayBeT<int?, Lazy<MayBe<int?>>>().Value.Value.IsEqual<int?>(null);
+
+            value2 = 3;
+            res = Sum(value1.AsMayBeT(new MayBeTransformer2.LazyMonad()), value2.AsMayBeT(new MayBeTransformer2.LazyMonad()));
+            res.RunMayBeT<int?, Lazy<MayBe<int?>>>().Value.Value.IsEqual<int?>(5);
+
+            //value1 = null;
+            //res = Sum(f1.AsLazy(), f2.AsLazy());
+            //res.Value.Value.IsEqual<int?>(null);
+            //value1GetCount.IsEqual(4);
+            //value2GetCount.IsEqual(2);
+
+            //SumWhere(f1.AsLazy(), f2.AsLazy()).Value.Value.IsEqual<int?>(null);
+            //value1 = 4;
+            //SumWhere(f1.AsLazy(), f2.AsLazy()).Value.Value.IsEqual<int?>(7);
+            //value1 = 2;
+            //SumWhere(f1.AsLazy(), f2.AsLazy()).Value.Value.IsEqual<int?>(null);
+        }
+        MayBeT<int?> Sum(MayBeT<int?> a, MayBeT<int?> b) {
+            return from x in a
+                   from y in b
+                   select x + y;
+        }
+    }
+}
+namespace PureSharp.MayBeTransformer2 {
+    public interface IMonad {
+        object Unit<A>(A value);
+        object SelectMany<A, B>(object value, Func<A, object> f);
+    }
+    public class LazyMonad : IMonad {
+        object IMonad.SelectMany<A, B>(object value, Func<A, object> f) {
+            return PureSharp.LazyMonad.LazyExtensions.SelectMany<A, B>((Lazy<A>)value, x => (Lazy<B>)f(x));
+        }
+        object IMonad.Unit<A>(A value) {
+            return PureSharp.LazyMonad.LazyExtensions.AsLazy(value);
+        }
+    }
+    public struct MayBeT<A>(object innerMonadicValue, IMonad monad) {
+        public object InnerMonadicValue { get; private set; }
+        = innerMonadicValue;
+        public IMonad Monad { get; private set; }
+        = monad;
+    }
+    public static partial class MayBeTExtensions2 {
+        public static TMonad RunMayBeT<A, TMonad>(this MayBeT<A> source) {
+            return (TMonad)source.InnerMonadicValue;
+        }
+        public static MayBeT<A> AsMayBeT<A>(this A source, IMonad monad) {
+            return source.Unit(monad);
+        }
+        static MayBeT<A> Unit<A>(this A source, IMonad monad) {
+            return new MayBeT<A>(monad.Unit(source.AsMayBe()), monad);
+        }
+        static MayBeT<B> SelectMany<A, B>(this MayBeT<A> source, Func<A, MayBeT<B>> f) {
+            object result = source.Monad.SelectMany<MayBe<A>, MayBe<B>>(
+                source.InnerMonadicValue,
+                (MayBe<A> x) => {
+                    if(x.Value != null) {
+                        return f(x.Value).InnerMonadicValue;
+                    }
+                    return source.Monad.Unit(default(B).AsMayBe());
+                }
+            );
+            return new MayBeT<B>(result, source.Monad);
+        }
+
+        public static MayBeT<C> SelectMany<A, B, C>(this MayBeT<A> source, Func<A, MayBeT<B>> f, Func<A, B, C> resultSelector) {
+            return source.SelectMany(
+                outer => f(outer).SelectMany(
+                inner => resultSelector(outer, inner).Unit<C>(source.Monad)));
+        }
+        public static MayBeT<B> Select<A, B>(this MayBeT<A> source, Func<A, B> f) {
+            return source.SelectMany(x => f(x).Unit<B>(source.Monad));
+        }
+
+    }
+}
--- PureSharp/Monads/MayBeT.cs
+++ PureSharp/Monads/MayBeT.cs
@@ -3,7 +3,7 @@ using PureSharp.MayBeMonad2;
 using PureSharp.LazyMonad;
 
 namespace PureSharp.MayBeTransformer {
-    public static partial class MayBeTExtensions {
+    public static partial class LazyMayBeExtensions {
         public static Lazy<MayBe<A>> AsLazyM<A>(this A source) {
             return source.Unit();
         }
@@ -19,57 +19,5 @@ namespace PureSharp.MayBeTransformer {
                 x => (x.Value != null ? f(x.Value) : LazyMonad.LazyExtensions.AsLazy(MayBe2Extensions.Empty<B>()))
             );
         }
-    }
-}
-
-namespace PureSharp.MayBeTransformer2 {
-    public interface IMonad {
-        object Unit<A>(A value);
-        object SelectMany<A, B>(object value, Func<A, object> f);
-    }
-    public class LazyMonad : IMonad {
-        object IMonad.SelectMany<A, B>(object value, Func<A, object> f) {
-            return PureSharp.LazyMonad.LazyExtensions.SelectMany<A, B>((Lazy<A>)value, x => (Lazy<B>)f(x));
-        }
-        object IMonad.Unit<A>(A value) {
-            return PureSharp.LazyMonad.LazyExtensions.AsLazy(value);
-        }
-    }
-    public struct MayBeT<A>(object innerMonadicValue, IMonad monad) {
-        public object InnerMonadicValue { get; private set; } = innerMonadicValue;
-        public IMonad Monad { get; private set; } = monad;
-    }
-    public static partial class MayBeTExtensions2 {
-        public static TMonad RunMayBeT<A, TMonad>(this MayBeT<A> source) {
-            return (TMonad)source.InnerMonadicValue;
-        }
-        public static MayBeT<A> AsMayBeT<A>(this A source, IMonad monad) {
-            return source.Unit(monad);
-        }
-        static MayBeT<A> Unit<A>(this A source, IMonad monad) {
-            return new MayBeT<A>(monad.Unit(source.AsMayBe()), monad);
-        }
-        static MayBeT<B> SelectMany<A, B>(this MayBeT<A> source, Func<A, MayBeT<B>> f) {
-            object result = source.Monad.SelectMany<MayBe<A>, MayBe<B>>(
-                source.InnerMonadicValue,
-                (MayBe<A> x) => {
-                    if(x.Value != null) {
-                        return f(x.Value).InnerMonadicValue;
-                    }
-                    return source.Monad.Unit(default(B).AsMayBe());
-                }
-            );
-            return new MayBeT<B>(result, source.Monad);
-        }
-
-        public static MayBeT<C> SelectMany<A, B, C>(this MayBeT<A> source, Func<A, MayBeT<B>> f, Func<A, B, C> resultSelector) {
-            return source.SelectMany(
-                outer => f(outer).SelectMany(
-                inner => resultSelector(outer, inner).Unit<C>(source.Monad)));
-        }
-        public static MayBeT<B> Select<A, B>(this MayBeT<A> source, Func<A, B> f) {
-            return source.SelectMany(x => f(x).Unit<B>(source.Monad));
-        }
-
     }
 }
