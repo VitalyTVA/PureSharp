fe6179032a213971ada48148f07b05e2a9f06f56
Vitaly Tolstikov <vitalytva@yanex.ru>
2014-10-05 22:03:19 -0700

    t

--- PureSharp/Monads/MonadExtensions.cs
+++ PureSharp/Monads/MonadExtensions.cs
@@ -113,6 +113,21 @@ namespace PureSharp.LazyMayBeMonad2 {
     }
 }
 namespace PureSharp.LazyMayBeMonad {
+    partial class LazyMayBeExtensions {
+        public static LazyMayBe<C> SelectMany<A, B, C>(this LazyMayBe<A> source, Func<A, LazyMayBe<B>> f, Func<A, B, C> resultSelector) {
+            return source.SelectMany(
+                outer => f(outer).SelectMany(
+                inner => resultSelector(outer, inner).Unit<C>()));
+        }
+        public static LazyMayBe<B> Select<A, B>(this LazyMayBe<A> source, Func<A, B> f) {
+            return source.SelectMany(x => f(x).Unit<B>());
+        }
+        public static LazyMayBe<A> Where<A>(this LazyMayBe<A> source, Func<A, bool> f) {
+            return source.SelectMany(x => f(x) ? x.Unit<A>() : Empty<A>());
+        }
+    }
+}
+namespace PureSharp.LazyMayBeMonad {
     public struct LazyMayBe<A>(Lazy<A> value) {
         public readonly Lazy<A> Value = value;
     }
@@ -135,21 +150,6 @@ namespace PureSharp.LazyMayBeMonad {
                 source.Value,
                 x => (x != null ? f(x).Value : LazyMonad.LazyExtensions.Unit(MayBeExtensions.Empty<B>()))
             ).AsLazyMayBe();
-        }
-    }
-}
-namespace PureSharp.LazyMayBeMonad {
-    partial class LazyMayBeExtensions {
-        public static LazyMayBe<C> SelectMany<A, B, C>(this LazyMayBe<A> source, Func<A, LazyMayBe<B>> f, Func<A, B, C> resultSelector) {
-            return source.SelectMany(
-                outer => f(outer).SelectMany(
-                inner => resultSelector(outer, inner).Unit<C>()));
-        }
-        public static LazyMayBe<B> Select<A, B>(this LazyMayBe<A> source, Func<A, B> f) {
-            return source.SelectMany(x => f(x).Unit<B>());
-        }
-        public static LazyMayBe<A> Where<A>(this LazyMayBe<A> source, Func<A, bool> f) {
-            return source.SelectMany(x => f(x) ? x.Unit<A>() : Empty<A>());
         }
     }
 }
